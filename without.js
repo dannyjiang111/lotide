0554925a75f3ab212e7874d47c9ff1b391918d7d
Danny Jiang <dannyjiang11@outlook.com>
2022-04-06 06:44:09 +0000

    added without function

---
+++ without.js
@@ -0,0 +1,30 @@
+const eqArrays = function(actual, eactualpected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== eactualpected[i]) {
+      return false;
+    }
+  } 
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const without = function (source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemsToRemove[i]) {
+      result.push(source[i]);
+    }
+  } return result;
+};
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
