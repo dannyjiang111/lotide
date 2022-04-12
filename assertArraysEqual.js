30277f0aea4dba1c0d61cd98f51cc818ccb1ba00
Danny Jiang <dannyjiang11@outlook.com>
2022-04-12 05:44:27 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,4 @@
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  } 
-  return true;
-};
+const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = function(actual,expected) {
   if (eqArrays(actual,expected)) {
@@ -14,3 +7,5 @@ const assertArraysEqual = function(actual,expected) {
     console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
   }
 };
+
+module.exports = assertArraysEqual;
